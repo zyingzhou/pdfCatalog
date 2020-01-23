dd39fcf63780013a7b89acb835bec7fa8fa36689
zhiying <m18897956307@163.com>
2020-01-23 17:55:36 +0800

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# coding:utf-8
+"""
+Author: zhiying
+关注志颖博客(www.zhouzying.cn),一起探讨爬虫技术！
+Date: 20-1-23 下午3:43
+Description: Initialize the pdfBookmark package
+
+"""
+__all__ = ["pdfBookmark"]
