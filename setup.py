63e43ebfd12c1cb3021ff7ab6f3ad83f60604f46
zhiying <m18897956307@163.com>
2020-01-23 17:56:30 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# coding:utf-8
+"""
+Author: zhiying
+关注志颖博客(www.zhouzying.cn),一起探讨爬虫技术！
+Date: 20-1-22 下午10:16
+Description: package setup
+
+"""
+from setuptools import setup
+
+
+def read_description():
+    with open('./README.md') as readme:
+        return readme.read()
+
+
+setup(
+    name='pdfBookmark',
+    version='1.0.0beta7',
+    description='Build bookmarks for PDF documents automatically.',
+    long_description=read_description(),
+    author='zhiying zhou',
+    author_email='zhiyingstatham@qq.com',
+    url='https://github.com/zyingzhou/pdfBookmark',
+    license='GNU Affero General Public License v3.0',
+    install_requires="pymupdf",
+
+    py_modules=['pdfBookmark', 'fitz'],
+    tests_require=['pytest'],
+    entry_points={'console_scripts': ['pdfBookmark = pdfBookmark:main']}
+
+)
