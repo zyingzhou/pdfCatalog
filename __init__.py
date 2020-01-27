a296d8d0f29c2ca1c8b95b204f9aec9d21b592d0
zhiying <m18897956307@163.com>
2020-01-27 16:10:26 +0800

    fixed some typos

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 Author: zhiying
 关注志颖博客(www.zhouzying.cn),一起探讨爬虫技术！
 Date: 20-1-23 下午3:43
-Description: Initialize the pdfBookmark package
+Description: Initialize the pdfCatalog package
 
 """
 __all__ = ["pdfCatalog"]
