d43da2bd09c708a2cca96d3f7473b0bb357ce952
zhiying <m18897956307@163.com>
2020-03-07 15:17:55 +0800

    added the test of first bookmark level

--- test/test_pdfCatalog.py
+++ test/test_pdfCatalog.py
@@ -11,6 +11,7 @@ import fitz
 from pdfCatalog import read_file
 from pdfCatalog import parse_file
 from pdfCatalog import check_bookmark
+from pdfCatalog import _check_first_bookmark_level
 
 path = '../pdfs/catalog.txt'
 file = '../pdfs/现代通信网.pdf'
@@ -24,11 +25,14 @@ def test_read_file():
 
 
 def test_parse_file():
+    catalog = []
     for line in read_file(path):
         title, book_mark_level, page_number = parse_file(line)
+        catalog.append([book_mark_level, title, page_number + offset])
         assert len(title) != 0
         assert book_mark_level is '1' or '2'
         assert type(page_number) is int
+    assert _check_first_bookmark_level(catalog) is False
 
 
 def test_check_bookmark():
@@ -36,10 +40,3 @@ def test_check_bookmark():
     bookmark_list = check_bookmark(doc)
     assert type(bookmark_list) is list
 
-
-def test_first_bookmark_level():
-    catalog = []
-    for line in read_file(path):
-        title, book_mark_level, page_number = parse_file(line)
-        catalog.append([book_mark_level, title, page_number + offset])
-    assert type(catalog[0]) is list and catalog[0][0] == 1
